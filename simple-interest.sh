03913252d953c4aa57262e8625086b2e06360cef
angelankita2007-bit <angelankita2007@gmail.com>
2026-02-12 11:09:04 +0530

    Update simple-interest.sh
    Signed-off-by: angelankita2007-bit <angelankita2007@gmail.com>

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,1 +1,14 @@
- #!/bin/bash
+#!/bin/bash
+# This script calculates simple interest.
+
+echo "Enter the principal:"
+read p
+echo "Enter rate of interest per year:"
+read r
+echo "Enter time period in years:"
+read t
+
+# Formula: s = (p * r * t) / 100
+s=$(echo "scale=2; $p * $r * $t / 100" | bc)
+
+echo "The simple interest is: $s"
